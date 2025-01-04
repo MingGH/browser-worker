b355b0e1ff0444f204de3383ff800e04605c07e2
MingGH <1558772318@qq.com>
2025-01-04 19:26:49 +0800

    抓取html内容

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 
-通过puppeteer抓取页面的html
+集成itty-router，增加api-key校验，通过puppeteer抓取页面的html
--- src/index.js
+++ src/index.js
@@ -1,40 +1,74 @@
+import { AutoRouter } from 'itty-router'
 import puppeteer from "@cloudflare/puppeteer";
 
-export default {
-	async fetch(request, env) {
-		/**
-		 * @param {string} PRESHARED_AUTH_HEADER_KEY Custom header to check for key
-		 * @param {string} PRESHARED_AUTH_HEADER_VALUE Hard coded key value
-		 */
-		const PRESHARED_AUTH_HEADER_KEY = "API-KEY";
-		const PRESHARED_AUTH_HEADER_VALUE = "1574e7b2-9bc8-4204-a2ee-88a88a7f1558";
-		const psk = request.headers.get(PRESHARED_AUTH_HEADER_KEY);
+const router = AutoRouter()
 
-		console.info("PRESHARED_AUTH_HEADER_VALUE" , psk);
-		if (psk === PRESHARED_AUTH_HEADER_VALUE) {
-			return handleRequest(request, env);
-		}else {
-			// Incorrect key supplied. Reject the request.
-			return new Response("Sorry, you have supplied an invalid key.", {
-				status: 403,
-			});
-		}
-	},
-};
+router.get('/fetchHtml', (request, env) => {
+	return fetchHtml(request, env)
+})
+
+// 处理根路径请求
+router.get('/', () => {
+	const html = `
+    <!DOCTYPE html>
+    <html>
+      <head><title>Home</title></head>
+      <body><h1>Welcome to the Home Page</h1></body>
+    </html>
+  `
+	return new Response(html, { headers: { 'Content-Type': 'text/html' } })
+})
 
 
-async function handleRequest(request, env){
-	const { searchParams } = new URL(request.url);
-	let url = searchParams.get("url");
+// 处理未匹配的请求 (404)
+router.all('*', () => {
+	const html = `
+    <!DOCTYPE html>
+    <html>
+      <head><title>404 Not Found</title></head>
+      <body><h1>404 - Page Not Found</h1></body>
+    </html>
+  `
+	return new Response(html, { headers: { 'Content-Type': 'text/html' }, status: 404 })
+})
 
-	const browser = await puppeteer.launch(env.MYBROWSER);
-	const page = await browser.newPage();
-	await page.goto(url);
-	const pageHtml = await page.content();
-	await browser.close();
-	return new Response(pageHtml, {
-		headers: {
-			'Content-Type': 'text/html',
-		},
-	});
+
+export default { ...router } // this looks pointless, but trust us
+
+
+
+
+async function fetchHtml(request, env) {
+	/**
+	 * @param {string} PRESHARED_AUTH_HEADER_KEY Custom header to check for key
+	 * @param {string} PRESHARED_AUTH_HEADER_VALUE Hard coded key value
+	 */
+	const PRESHARED_AUTH_HEADER_KEY = "API-KEY";
+	const PRESHARED_AUTH_HEADER_VALUE = "1574e7b2-9bc8-4204-a2ee-88a88a7f1558";
+	const psk = request.headers.get(PRESHARED_AUTH_HEADER_KEY);
+
+	console.info("PRESHARED_AUTH_HEADER_VALUE" , psk);
+	if (psk === PRESHARED_AUTH_HEADER_VALUE) {
+		const { searchParams } = new URL(request.url);
+		let url = searchParams.get("url");
+
+		const browser = await puppeteer.launch(env.MYBROWSER);
+		const page = await browser.newPage();
+		await page.goto(url);
+		const pageHtml = await page.content();
+		await browser.close();
+		return new Response(pageHtml, {
+			headers: {
+				'Content-Type': 'text/html',
+			},
+		});
+	}else {
+		// Incorrect key supplied. Reject the request.
+		return new Response("Sorry, you have supplied an invalid key.", {
+			status: 403,
+		});
+	}
 }
+
+
+
